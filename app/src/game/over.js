61ed59a5f17fb33efc981eba09ecd437c4a6d539
Andre Sch <andresch.dev@gmail.com>
2021-09-08 21:28:33 -0300

    Display and hide GameOver

--- app/src/game/over.js
+++ app/src/game/over.js
@@ -1,4 +1,44 @@
 const GameOver = {
+  display(type, winner) {
+    const img = document.createElement('img')
+
+    img.src = `${staticProvider}/assets/winner/${winner}.svg`
+    img.alt = winner + ' wins'
+    img.draggable = false
+
+    document.getElementById('winner-container').appendChild(img)
+
+    const h1 = document.querySelector('#game-over .text h1')
+    const h2 = document.querySelector('#game-over .text h2')
+
+    if (winner == 'nobody') {
+      h1.innerHTML = 'draw'
+    } else {
+      h1.innerHTML = winner + ' team wins'
+    }
+    h2.innerHTML = type.toUpperCase()
+
+    BodyOverlayInModals.display(this.hide)
+
+    const enabledButtons = document.querySelectorAll(
+      '.chess-square button.player-of-turn'
+    )
+    for (const squareButton of enabledButtons) {
+      PlayerActions.remove(squareButton)
+    }
+
+    document.getElementById('game-over').style.display = 'flex'
+  },
+  hide() {
+    document.getElementById('winner-container').innerHTML = ''
+
+    document.querySelector('#game-over .text h1').textContent = ''
+    document.querySelector('#game-over .text h2').textContent = ''
+
+    BodyOverlayInModals.hide()
+
+    document.getElementById('game-over').style.display = 'none'
+  },
   resetGame() {
     document.getElementById('turn-display').innerHTML = 'white turn'
     document.getElementById('king-check').innerHTML = ''
